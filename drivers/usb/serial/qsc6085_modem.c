9d68bf127b379e0e78411b8b4a6a90d586172aee
kfazz <kfazz01@gmail.com>
2013-02-20 10:15:11 -0500

    set wakeup_gpio to -1 to disable, bpwake driver handles it instead, which also keeps uart0 alive

--- drivers/usb/serial/qsc6085_modem.c
+++ drivers/usb/serial/qsc6085_modem.c
@@ -1486,7 +1486,7 @@ static void __exit modem_exit(void)
 static int __init modem_init(void)
 {
 	int retval = 0;
-	//modem_remote_wake_gpio = SHOLES_APWAKE_TRIGGER_GPIO; //using bpwake instead, it wakes ttyS0 too
+	modem_remote_wake_gpio = -1; // SHOLES_APWAKE_TRIGGER_GPIO; //using bpwake instead, it wakes ttyS0 too
 	retval = usb_serial_register(&modem_device);
 	if (retval)
 		return retval;
